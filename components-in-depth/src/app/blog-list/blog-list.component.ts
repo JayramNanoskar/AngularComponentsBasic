0b0ad382d942d53859d384a68acc20c334d8c804
JayramNanoskar <jayram.n978@gmail.com>
2020-11-18 10:23:08 +0530

    using lifecycle hooks

--- components-in-depth/src/app/blog-list/blog-list.component.ts
+++ components-in-depth/src/app/blog-list/blog-list.component.ts
@@ -8,9 +8,13 @@ import { BlogPost } from '../blog-post';
 })
 export class BlogListComponent implements OnInit {
   blogPosts: BlogPost[] = [];
+  
+  //ideally constructor need to keep lightweight, used for dependency injection
+  //other stuff should be in ngOnInit() not in constructor() like - regarding initializing instances and also making rest service calls
+  //because it is easiar for component testing(inside test classes) - to create new instances for component to run test on it 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // one of angular lifecycle events marks by certain hooks(point)
     this.blogPosts.push(new BlogPost("Blog post 1", "Summary 1"));
     this.blogPosts.push(new BlogPost("Blog post 2", "Summary 2"));
     this.blogPosts.push(new BlogPost("Blog post 3", "Summary 3"));
