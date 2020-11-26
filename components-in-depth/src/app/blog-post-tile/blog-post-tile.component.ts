0a519eddb80557b31abd222c992ef2722747f464
JayramNanoskar <jayram.n978@gmail.com>
2020-11-26 23:23:12 +0530

    using view encapsulation

--- components-in-depth/src/app/blog-post-tile/blog-post-tile.component.ts
+++ components-in-depth/src/app/blog-post-tile/blog-post-tile.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { BlogPost } from '../blog-post';
 import { TruncatePipe } from '../truncate.pipe';
 
 @Component({
   selector: 'app-blog-post-tile',
   templateUrl: './blog-post-tile.component.html',
-  styleUrls: ['./blog-post-tile.component.scss']
+  styleUrls: ['./blog-post-tile.component.scss'],
+  //encapsulation: ViewEncapsulation.None //bydefault Emulated
+  //none- stops encapsulation- won't crete extra attribute on element at runtime
 })
 export class BlogPostTileComponent implements OnInit {
   @Input() post: BlogPost; // type of input member variable of child component should be same as the type being passed in by the parent component
