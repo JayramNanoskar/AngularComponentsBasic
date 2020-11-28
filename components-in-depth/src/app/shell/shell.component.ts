32502b76e1455fa1c38a08d8886bf2b84eb6de83
JayramNanoskar <jayram.n978@gmail.com>
2020-11-28 22:31:24 +0530

    using ContentChild to project content in component

--- components-in-depth/src/app/shell/shell.component.ts
+++ components-in-depth/src/app/shell/shell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shell',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.scss']
 })
 export class ShellComponent implements OnInit {
-
+  //cannot use @viewChild to access ng-content(app component indirectly) element/data here as this(shell component) does not have it as direct child 
+  @ContentChild('appHeader') headerElement; //getting element from Template reference variable
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentChecked(){
+    // console.log(this.headerElement.nativeElement);//<h4 _ngcontent-pqf-c17 header>Angular Blogs</h4>
   }
 
 }
